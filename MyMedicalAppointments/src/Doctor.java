4fb36b53c8f515a6ac2e181fe9795e49e1a80aad
Wilmer Arteaga <killerwilmer@gmail.com>
2019-12-05 23:03:01 -0500

    super and this (inheritance)

--- MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppointments/src/Doctor.java
@@ -1,40 +1,11 @@
 import java.util.ArrayList;
 import java.util.Date;
 
-public class Doctor {
-  static int id = 0;
-  private String name;
+public class Doctor extends User {
   private String speciality;
 
-  Doctor() {
-    System.out.println("Construyendo el objeto doctor");
-  }
-
-  Doctor(String name, String speciality) {
-    System.out.println("El nombre del Doctor asignado es: " + name);
-    id++;
-    this.name = name;
-    this.speciality = speciality;
-  }
-
-  public void showName() {
-    System.out.println(name);
-  }
-
-  public static int getId() {
-    return id;
-  }
-
-  public static void setId(int id) {
-    Doctor.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
+  Doctor(String name, String email) {
+    super(name, email);
   }
 
   public String getSpeciality() {
--- MyMedicalAppointments/src/Patient.java
+++ MyMedicalAppointments/src/Patient.java
@@ -1,48 +1,11 @@
-public class Patient {
-    private String name;
-    private String email;
-    private String address;
-    private String phoneNumber;
+public class Patient extends User {
     private String birthday;
     private double weight;
     private double height;
     private String blood;
 
     Patient(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+        super(name, email);
     }
 
     public String getBirthday() {
