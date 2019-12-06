277119f19b81f7c9d29dc6eb100bacbc72dc5ca9
Wilmer Arteaga <killerwilmer@gmail.com>
2019-12-05 22:31:58 -0500

    Enumerations

---
+++ MyMedicalAppointments/src/TestEnum.java
@@ -0,0 +1,31 @@
+public class TestEnum {
+  public enum Day {
+    SATURDAY("Sabado", "土曜日");
+
+    private String spanish;
+    private String japanese;
+
+    Day(String spanish, String japanese) {
+      this.spanish = spanish;
+      this.japanese = japanese;
+    }
+
+    public String getSpanish() {
+      return spanish;
+    }
+
+    public String getJapanese() {
+      return japanese;
+    }
+
+    public static void main(String[] args) {
+      Day day = Day.SATURDAY;
+
+      switch (day) {
+        case SATURDAY:
+          System.out.println("Spanish: " + day.getSpanish() + " Japanese: " + day.getJapanese());
+          break;
+      }
+    }
+  }
+}
