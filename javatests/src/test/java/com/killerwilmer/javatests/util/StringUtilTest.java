7492d188977b8b5933bd8e79c529ed338b28e00d
Wilmer Arteaga <killerwilmer@gmail.com>
2019-12-22 12:18:59 -0500

    added StringUtil and StringUtilTest basic

---
+++ javatests/src/test/java/com/killerwilmer/javatests/util/StringUtilTest.java
@@ -0,0 +1,10 @@
+package com.killerwilmer.javatests.util;
+
+public class StringUtilTest {
+
+  public static void main(String[] args) {
+
+    String result = StringUtil.repeat("hola", 3);
+    System.out.println(result);
+  }
+}
