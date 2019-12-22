50f884f54b20679b49ae980a8bff0df7accb9c67
Wilmer Arteaga <killerwilmer@gmail.com>
2019-12-22 16:47:56 -0500

    Simulate (mock) class Dice with Mockito.mock

---
+++ javatests/src/test/java/com/killerwilmer/javatests/player/PlayerTest.java
@@ -0,0 +1,27 @@
+package com.killerwilmer.javatests.player;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+  @Test
+  public void test_looses_when_dice_number_is_too_low() {
+    Dice dice = Mockito.mock(Dice.class);
+    Mockito.when(dice.roll()).thenReturn(2);
+
+    Player player = new Player(dice, 3);
+    assertFalse(player.play());
+  }
+
+  @Test
+  public void test_wins_when_dice_number_is_big() {
+    Dice dice = Mockito.mock(Dice.class);
+    Mockito.when(dice.roll()).thenReturn(4);
+
+    Player player = new Player(dice, 3);
+    assertTrue(player.play());
+  }
+}
