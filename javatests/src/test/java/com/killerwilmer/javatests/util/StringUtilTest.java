8fe295a06994063f3dc6a0c5d161d2380db8d96c
Wilmer Arteaga <killerwilmer@gmail.com>
2019-12-22 15:08:10 -0500

    organize the test, once test for method

--- javatests/src/test/java/com/killerwilmer/javatests/util/StringUtilTest.java
+++ javatests/src/test/java/com/killerwilmer/javatests/util/StringUtilTest.java
@@ -6,9 +6,26 @@ import org.junit.Test;
 public class StringUtilTest {
 
   @Test
-  public void testRepeat() {
+  public void test_repeat_string_once() {
+
+    Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
+  }
+
+  @Test
+  public void test_repeat_string_multiple_times() {
 
     Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
-    Assert.assertEquals("hola", StringUtil.repeat("hola", 1));
+  }
+
+  @Test
+  public void test_repeat_string_zero_times() {
+
+    Assert.assertEquals("", StringUtil.repeat("hola", 0));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void test_repeat_string_negative_times() {
+
+    StringUtil.repeat("hola", -1);
   }
 }
