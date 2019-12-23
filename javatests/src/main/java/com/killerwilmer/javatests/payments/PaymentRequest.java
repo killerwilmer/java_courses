7f2a846d9bac85724e6fccd33a554eb46959f19f
Wilmer Arteaga <killerwilmer@gmail.com>
2019-12-22 23:30:02 -0500

    payment gateway mock example

---
+++ javatests/src/main/java/com/killerwilmer/javatests/payments/PaymentRequest.java
@@ -0,0 +1,14 @@
+package com.killerwilmer.javatests.payments;
+
+public class PaymentRequest {
+
+  private double amount;
+
+  public PaymentRequest(double amount) {
+    this.amount = amount;
+  }
+
+  public double getAmount() {
+    return amount;
+  }
+}
---
+++ javatests/src/main/java/com/killerwilmer/javatests/payments/PaymentResponse.java
@@ -0,0 +1,19 @@
+package com.killerwilmer.javatests.payments;
+
+public class PaymentResponse {
+
+  enum PaymentStatus {
+    OK,
+    ERROR
+  }
+
+  private PaymentStatus status;
+
+  public PaymentResponse(PaymentStatus status) {
+    this.status = status;
+  }
+
+  public PaymentStatus getStatus() {
+    return status;
+  }
+}
