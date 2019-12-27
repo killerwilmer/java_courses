4eb4ae2b1484fdddd1384ae67bbe886195e9bbb8
Wilmer Arteaga <killerwilmer@gmail.com>
2019-12-26 21:22:08 -0500

    test for discounts

---
+++ javatests/src/test/java/com/killerwilmer/javatests/discounts/PriceCalculatorTest.java
@@ -0,0 +1,40 @@
+package com.killerwilmer.javatests.discounts;
+
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class PriceCalculatorTest {
+
+  @Test
+  public void total_zero_when_there_are_prices() {
+
+    PriceCalculator calculator = new PriceCalculator();
+
+    assertThat(calculator.getTotal(), is(0.0));
+  }
+
+  @Test
+  public void total_is_the_sum_of_price() {
+
+    PriceCalculator calculator = new PriceCalculator();
+
+    calculator.addPrice(10.2);
+    calculator.addPrice(15.5);
+
+    assertThat(calculator.getTotal(), is(25.7));
+  }
+
+  @Test
+  public void apply_discount_to_prices() {
+    PriceCalculator calculator = new PriceCalculator();
+
+    calculator.addPrice(100.0);
+    calculator.addPrice(50.0);
+    calculator.addPrice(50.0);
+
+    calculator.setDiscount(25);
+
+    assertThat(calculator.getTotal(), is(150.0));
+  }
+}
