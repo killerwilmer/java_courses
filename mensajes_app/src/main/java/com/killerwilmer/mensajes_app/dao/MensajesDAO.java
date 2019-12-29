9614855664c68b2c460e98cea1ce5c80e0087347
Wilmer Arteaga <killerwilmer@gmail.com>
2019-12-29 15:49:10 -0500

    create new message

--- mensajes_app/src/main/java/com/killerwilmer/mensajes_app/dao/MensajesDAO.java
+++ mensajes_app/src/main/java/com/killerwilmer/mensajes_app/dao/MensajesDAO.java
@@ -1,10 +1,35 @@
 package com.killerwilmer.mensajes_app.dao;
 
 import com.killerwilmer.mensajes_app.model.Mensajes;
+import com.killerwilmer.mensajes_app.util.Conexion;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class MensajesDAO {
 
-  public static void crearMensajeDB(Mensajes mensaje) {}
+  public static void crearMensajeDB(Mensajes mensaje) {
+    Conexion db_connect = new Conexion();
+
+    try {
+      Connection conexion = db_connect.get_connection();
+      PreparedStatement ps = null;
+      try {
+        String query = "INSERT INTO mensajes ( mensaje, autor_mensaje) VALUES (?,?)";
+        ps = conexion.prepareStatement(query);
+        ps.setString(1, mensaje.getMensaje());
+        ps.setString(2, mensaje.getAutor_mensaje());
+        ps.executeUpdate();
+        System.out.println("mensaje creado");
+
+      } catch (SQLException ex) {
+        System.out.println(ex);
+      }
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
 
   public static void leerMensajesDB() {}
 
--- mensajes_app/src/main/java/com/killerwilmer/mensajes_app/service/MensajesService.java
+++ mensajes_app/src/main/java/com/killerwilmer/mensajes_app/service/MensajesService.java
@@ -1,8 +1,25 @@
 package com.killerwilmer.mensajes_app.service;
 
+import com.killerwilmer.mensajes_app.dao.MensajesDAO;
+import com.killerwilmer.mensajes_app.model.Mensajes;
+
+import java.util.Scanner;
+
 public class MensajesService {
 
-  public static void crearMensaje() {}
+  public static void crearMensaje() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Escribe tu mensaje");
+    String mensaje = sc.nextLine();
+
+    System.out.println("tu nombre");
+    String nombre = sc.nextLine();
+
+    Mensajes registro = new Mensajes();
+    registro.setMensaje(mensaje);
+    registro.setAutor_mensaje(nombre);
+    MensajesDAO.crearMensajeDB(registro);
+  }
 
   public static void listarMensajes() {}
 
