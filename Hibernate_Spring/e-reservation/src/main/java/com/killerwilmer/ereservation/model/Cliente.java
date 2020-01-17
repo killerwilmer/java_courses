192ce68d93b32da93f59cf7287ff87f29c1ce695
Wilmer <killerwilmer@misena.edu.co>
2020-01-17 16:07:41 -0500

    Create first class with lombok

---
+++ Hibernate_Spring/e-reservation/src/main/java/com/killerwilmer/ereservation/model/Cliente.java
@@ -0,0 +1,13 @@
+package com.killerwilmer.ereservation.model;
+
+import lombok.Data;
+
+/** @author killerwilmer */
+@Data
+public class Cliente {
+
+  private String nombreCli;
+  private String apellidoCli;
+
+  public Cliente() {}
+}
