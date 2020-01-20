61433674d975050d306b2a009f7f95b4514aa4b0
Wilmer <killerwilmer@misena.edu.co>
2020-01-20 13:32:37 -0500

    How use @Transactional

--- Hibernate_Spring/e-reservation/src/main/java/com/killerwilmer/ereservation/controller/services/ClienteService.java
+++ Hibernate_Spring/e-reservation/src/main/java/com/killerwilmer/ereservation/controller/services/ClienteService.java
@@ -3,9 +3,11 @@ package com.killerwilmer.ereservation.controller.services;
 import com.killerwilmer.ereservation.controller.repository.ClienteRepository;
 import com.killerwilmer.ereservation.model.Cliente;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /** @author killerwilmer */
 @Service
+@Transactional(readOnly = true)
 public class ClienteService {
 
   private final ClienteRepository clienteRepository;
@@ -14,14 +16,17 @@ public class ClienteService {
     this.clienteRepository = clienteRepository;
   }
 
+  @Transactional
   public Cliente create(Cliente cliente) {
     return this.clienteRepository.save(cliente);
   }
 
+  @Transactional
   public Cliente update(Cliente cliente) {
     return this.clienteRepository.save(cliente);
   }
 
+  @Transactional
   public void delete(Cliente cliente) {
     this.clienteRepository.delete(cliente);
   }
