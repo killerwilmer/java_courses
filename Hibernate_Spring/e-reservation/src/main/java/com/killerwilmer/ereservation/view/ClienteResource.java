c8098c40fbd7874bf7d83cad21c5a2698ed16a40
Wilmer <killerwilmer@misena.edu.co>
2020-01-20 16:09:40 -0500

    add documentation annotations for the API

--- Hibernate_Spring/e-reservation/src/main/java/com/killerwilmer/ereservation/view/ClienteResource.java
+++ Hibernate_Spring/e-reservation/src/main/java/com/killerwilmer/ereservation/view/ClienteResource.java
@@ -3,6 +3,10 @@ package com.killerwilmer.ereservation.view;
 import com.killerwilmer.ereservation.controller.services.ClienteService;
 import com.killerwilmer.ereservation.model.Cliente;
 import com.killerwilmer.ereservation.view.vo.ClienteVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +16,7 @@ import java.util.List;
 /** @author killerwilmer */
 @RestController
 @RequestMapping("/api/cliente")
+@Api(tags = "cliente")
 public class ClienteResource {
 
   private final ClienteService clienteService;
@@ -21,6 +26,12 @@ public class ClienteResource {
   }
 
   @PostMapping
+  @ApiOperation(value = "Crear Cliente", notes = "Servicio para crear un nuevo Cliente")
+  @ApiResponses(
+      value = {
+        @ApiResponse(code = 201, message = "Cliente creado correctamente"),
+        @ApiResponse(code = 400, message = "Solicitud Inválida")
+      })
   public ResponseEntity<Cliente> createCliente(@RequestBody ClienteVO clienteVO) {
 
     Cliente cliente = new Cliente();
@@ -34,6 +45,12 @@ public class ClienteResource {
   }
 
   @PutMapping("/{identificacion}")
+  @ApiOperation(value = "Actualizar Cliente", notes = "Servicio para actualizar un cliente")
+  @ApiResponses(
+      value = {
+        @ApiResponse(code = 201, message = "Cliente actualizado correctamente"),
+        @ApiResponse(code = 404, message = "Cliente no encontrado")
+      })
   public ResponseEntity<Cliente> updateCliente(
       @PathVariable("identificacion") String identificacion, ClienteVO clienteVO) {
 
@@ -52,6 +69,12 @@ public class ClienteResource {
   }
 
   @DeleteMapping("/{identificacion}")
+  @ApiOperation(value = "Eliminar Cliente", notes = "Servicio para eliminar un cliente")
+  @ApiResponses(
+      value = {
+        @ApiResponse(code = 201, message = "Cliente eliminado correctamente"),
+        @ApiResponse(code = 404, message = "Cliente no encontrado")
+      })
   public ResponseEntity<Cliente> removeCliente(
       @PathVariable("identificacion") String identificacion) {
 
@@ -66,6 +89,12 @@ public class ClienteResource {
   }
 
   @GetMapping
+  @ApiOperation(value = "Listar Clientes", notes = "Servicio para listar todos los clientes")
+  @ApiResponses(
+      value = {
+        @ApiResponse(code = 201, message = "Clientes encontrados"),
+        @ApiResponse(code = 404, message = "Clientes no encontrados")
+      })
   public ResponseEntity<List<Cliente>> findAll() {
 
     return ResponseEntity.ok(this.clienteService.findAll());
